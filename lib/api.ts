39c6f2fb17fc8d3da3726560572589ef33a0226b
Bonifasius Martin <martinwibawa@gmail.com>
2026-01-23 13:23:59 +0700

    Update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -3,7 +3,7 @@ export async function getOdometerHistory(
   range: string = "1h"
 ) {
   const res = await fetch(
-    `http://localhost:4000/api/telemetry/odometer?vehicleId=${vehicleId}&range=${range}`
+    `https://riset-be.mpiskawe.my.id/api/telemetry/odometer?vehicleId=${vehicleId}&range=${range}`,
   );
   if (!res.ok) throw new Error("Failed to fetch odometer history");
   const fullResponse = await res.json();
